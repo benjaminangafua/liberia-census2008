8566abfffc713533b71d63a5470837914b9ef1a7
BenjaminANgafua <benjaminarkutl2017@gmail.com>
2021-06-09 16:53:45 +0000

    census latest update

--- script.js
+++ script.js
@@ -3,90 +3,61 @@ fetch("./census.json")
     .then((data) => {
         const POPULATION = data.population;
 
-        //Get each county total number of population
-        POPULATION.map((element) => {
-
-            // console.log(element.county)
-
-            // if (element.county) console.log(element.male + element.female)
-
-        });
-
-        var initialValue = 0;
         //Computing total female
-        let total_female = POPULATION.reduce(function(accumulator, currentValue) {
-                return accumulator + currentValue.female
-            }, initialValue)
+        let total_female = POPULATION.reduce((acc, value) => (acc + value.female), 0)
             //Print total  Female 
-        document.getElementById("fem").insertAdjacentHTML("beforebegin", ` <b>${total_female}</b>`)
+        document.getElementById("femaleData").insertAdjacentHTML("beforebegin", ` <b>${total_female}</b>`)
 
         //Computing total male
-        let total_male = POPULATION.reduce(function(accumulator, currentValue) {
-            return accumulator + currentValue.male
-        }, initialValue)
-
-        //Print total male 
+        let total_male = POPULATION.reduce((acc, value) => (acc + value.male), 0)
         document.getElementById("mal").insertAdjacentHTML("beforebegin", `<b> ${total_male}</b>`)
 
         //Computing total population
         let total_population = total_male + total_female;
-
-        // Print total population
         document.getElementById("popu").insertAdjacentHTML("afterbegin", `<b>${total_population}</b>`)
 
-        /*get the array of counties without duplicate for the label on the chart
-        let get_county_array = POPULATION.map(function(counties) {
-            return counties.county;
-        })
-        let county = get_county_array.filter((county, index) => get_county_array.indexOf(county) === index)
-        */
-        let county = POPULATION.reduce(function(accumulator, currentValue) {
-            if (accumulator.indexOf(currentValue.county) === -1) {
-                accumulator.push(currentValue.county)
+        //get the array of counties without duplicate for the label on the chart
+        let county = POPULATION.reduce((acc, value) => {
+            if (acc.indexOf(value.county) === -1) {
+                acc.push(value.county)
             }
-            return accumulator
+            return acc
         }, [])
 
+
+
+        // let district_option = document.querySelector("#disOptn")
+        // district_option.addEventListener("change", (ele) => {
+
+        // })
+        // county.forEach((c) => {
+        //     district_option.insertAdjacentHTML("beforeend", `<option>${c}</option>
+        // `)
+        // })
+
+        let houses_option = document.querySelector("#housesOptn")
+            // district_option.addEventListener("change", (ele) => {
+
+        // })
+        county.forEach((c) => {
+            houses_option.insertAdjacentHTML("beforeend", `<option>${c}</option>
+        `)
+        })
+
+
         // Total number of population per county
-        let tots = POPULATION.reduce((a, c) => (a[c.county] = (a[c.county] || 0) + c.male + c.female, a), {})
-
-        console.log(tots);
-        // let countyPopTotal = POPULATION.reduce(function(accumulator, currentValue, index) {
-        //     if (accumulator.indexOf(currentValue.county) === -1) {
-
-        //         accumulator.push(currentValue.male + currentValue.female)
-        //     }
-        //     return accumulator
-        // }, [])
-        // console.log("Pop", countyPopTotal);
-
-
-        var CountyTotal = []
-        for (var i = 0; i < county.length; i++) {
-
-            let maleTotal, femaleTotal;
-            for (var j = i; j < POPULATION.length; j++) {
-                if (county[i] === POPULATION[j].county) {
-                    maleTotal = POPULATION[j].male
-                    femaleTotal = POPULATION[j].female
-                }
-            }
-            let total = maleTotal + femaleTotal
-            CountyTotal.push(total)
-        }
-        // console.log(CountyTotal)
-
+        let total_population_per_county = POPULATION.reduce((a, c) => (a[c.county] = (a[c.county] || 0) + c.male + c.female, a), {})
 
         // doughnutChart of total male and female
         new Chart(document.getElementById("doughnut-chart"), {
             type: 'doughnut',
             data: {
-                labels: ["MALE", "FEMALE"],
+                labels: ['MALE', 'FEMALE'],
                 datasets: [{
-                    label: "Population (millions)",
+                    label: "Gender",
+                    data: [total_male, total_female],
                     backgroundColor: ["#B1D2C2", "#F0F2EF"],
-                    data: [total_male, total_female]
-                }]
+                }],
             },
             options: {
                 plugins: {
@@ -95,7 +66,8 @@ fetch("./census.json")
                         position: "bottom",
                         borderRadius: 6,
                         labels: {
-                            usePointStyle: true
+                            usePointStyle: true,
+                            color: "#B1D2C2",
                         }
                     }
                 },
@@ -103,16 +75,14 @@ fetch("./census.json")
             }
         });
         //Bar Chart  for Distribution of Population by county
-        new Chart(document.getElementById("bar1-chart"), {
+        new Chart(document.getElementById("county-chart"), {
             type: 'bar',
             data: {
                 labels: county,
                 datasets: [{
-                    label: county.forEach((c) => {
-                        return c;
-                    }),
+                    label: "Population Per County",
                     backgroundColor: "#B1D2C2",
-                    data: tots, //data for labels (1st label)
+                    data: total_population_per_county, //data for labels (1st label)
                     borderRadius: 5,
                     width: 1,
                     barThickness: 18
@@ -122,32 +92,31 @@ fetch("./census.json")
                 scales: {
                     y: {
                         max: 1120000,
-                        min: 0,
+                        min: 10000,
                         ticks: {
-                            stepSize: 600000
+                            stepSize: 10000
                         }
                     }
                 },
                 plugins: {
                     legend: {
-                        display: false,
+                        display: true,
 
                     }
-                }
+                },
+                aspectRatio: 2.5
 
             }
         });
         //Bar Chart for Distribution of Population by District
-        new Chart(document.getElementById("bar-chart"), {
+        /*new Chart(document.getElementById("district"), {
             type: 'bar',
             data: {
                 labels: county,
                 datasets: [{
-                    label: county.forEach((c) => {
-                        return c;
-                    }),
+                    label: "Population Per District",
                     backgroundColor: "#B1D2C2",
-                    data: tots,
+                    data: total_population_per_county,
                     borderRadius: 5,
                     width: 1,
                     barThickness: 18
@@ -165,12 +134,43 @@ fetch("./census.json")
                 },
                 plugins: {
                     legend: {
-                        display: false,
+                        display: true,
                     }
+                }
+            },
+            aspectRatio: 2.5
+        });
+*/
+        new Chart(document.getElementById("district"), {
+            type: 'bar',
+            data: {
+                labels: county,
+                datasets: [{
+                    label: "Population Per District",
+                    backgroundColor: "#B1D2C2",
+                    data: total_population_per_county,
+                    borderRadius: 5,
+                    width: 1,
+                    barThickness: 18
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        max: 1200000,
+                        min: 0,
+                        ticks: {
+                            stepSize: 400000
+                        }
+                    }
+                },
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: 'Predicted world population (millions) in 2050'
                 }
             }
         });
-
         //Bar Chart  for Distribution of Population by Number of Households
         new Chart(document.getElementById("houses-chart"), {
             type: 'bar',
@@ -181,7 +181,7 @@ fetch("./census.json")
                         return c;
                     }),
                     backgroundColor: "#B1D2C2",
-                    data: tots,
+                    data: total_population_per_county,
                     borderRadius: 5,
                     width: 1,
                     barThickness: 18
@@ -199,12 +199,12 @@ fetch("./census.json")
                 },
                 plugins: {
                     legend: {
-                        display: false,
+                        display: true,
 
                     }
                 }
 
             }
         });
-
-    }).catch(error => console.log({ error }))
+    })
+    // .catch(error => console.log({ error }))
