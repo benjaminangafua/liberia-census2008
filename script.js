ce69d3a94604aa920a3ff3443b252fc50475e9d4
BenjaminANgafua <benjaminarkutl2017@gmail.com>
2021-06-11 13:16:07 +0000

    census latest update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let district_option = document.querySelector("#disOptn")
-
+let newPopulation = []
 fetch("./census.json")
     .then((response) => response.json())
     .then((data) => {
-        const POPULATION = data.population;
+        let real_data = data.population
+        newPopulation.push(real_data);
+        let POPULATION = data.population
 
         //Computing total female
         let total_female = POPULATION.reduce((acc, value) => (acc + value.female), 0)
@@ -148,36 +150,7 @@ fetch("./census.json")
             aspectRatio: 2.5
         });
         **/
-        new Chart(document.getElementById("district"), {
-            type: 'bar',
-            data: {
-                labels: county,
-                datasets: [{
-                    label: "Population Per District",
-                    backgroundColor: "#519872",
-                    data: total_population_per_county,
-                    borderRadius: 5,
-                    width: 1,
-                    barThickness: 18
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        max: 1200000,
-                        min: 0,
-                        ticks: {
-                            stepSize: 400000
-                        }
-                    }
-                },
-                legend: { display: false },
-                title: {
-                    display: true,
-                    text: 'Predicted Liberia population (millions) in 2008'
-                }
-            }
-        });
+
         //Bar Chart  for Distribution of Population by Number of Households
         new Chart(document.getElementById("houses-chart"), {
             type: 'bar',
@@ -213,26 +186,102 @@ fetch("./census.json")
         });
     })
     // .catch(error => console.log({ error }))
+console.log(newPopulation);
 
 function GetDistrict() {
-    console.log(district_option.value)
-    fetch('/census.json').then(res => res.json())
-        .then(data => {
-            const POPULATION_DATA = data.population
-            let findCounty = POPULATION_DATA.forEach(ele => {
-                console.log(`${ele.county}: ${ele.district}`);
-            })
-            console.log(findCounty)
-                // let findCounty = POPULATION_DATA.reduce((acc, value) => {
-                //     if (acc.indexOf(value.county) === -1) {
-                //         acc.push(`${acc[value.county]}${value.district}`)
-                //     }
-                //     return acc
-                // }, [])
+    let selected_county = district_option.value
+    console.log(selected_county)
+
+    let districts = [];
+    let disticts_population = []
+    newPopulation.forEach(ele => {
+        ele.forEach(elem => {
+            console.log("", elem.county);
+            if (elem.county == selected_county) {
+                let district = elem.district
+                districts.push(district)
+
+                let distict_population = elem.male + elem.female
+                disticts_population.push(distict_population)
+
+            }
         })
+    })
+    console.log(districts)
+    new Chart(document.getElementById("district"), {
+        type: 'bar',
+        data: {
+            labels: districts,
+            datasets: [{
+                label: "Population Per District",
+                backgroundColor: "#519872",
+                data: disticts_population,
+                borderRadius: 5,
+                width: 1,
+                barThickness: 18
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    max: 50000,
+                    min: 0,
+                    ticks: {
+                        stepSize: 1000
+                    }
+                }
+            },
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Predicted Liberia population (millions) in 2008'
+            }
+        }
+    });
 }
 
 // The five highest populated counties
 function FindHighestPopulationCounties() {
     "mostPopCoun"
+
+    // fetch('/census.json').then(res => res.json())
+    //     .then(data => {
+    //         const POPULATION_DATA = data.population
+    //         let most_district = POPULATION_DATA.reduce((acc, value) => {
+    //             const county = value.county;
+
+    //             if (!acc[county]) {
+    //                 acc[county] = {};
+    //             }
+    //             const distr = acc[county].district;
+    //             acc[county] = { district: distr };
+
+    //             // if()
+
+    //             return acc;
+    //         }, {})
+    //         console.log(most_district)
+    //     })
+    // fetch('/census.json').then(res => res.json())
+    //     .then(data => {
+    //         const POPULATION_DATA = data.population
+    //         let most_populat = POPULATION_DATA.reduce((acc, value) => {
+    //             const county = value.county;
+
+    //             if (!acc[county]) {
+    //                 acc[county] = {};
+    //             }
+    //             const countyMales = acc[county].male || 0;
+    //             const countyFemales = acc[county].female || 0;
+    //             const updatedMales = countyMales + value.male;
+    //             const updatedFemales = countyFemales + value.female;
+    //             acc[county] = { male: updatedMales, female: updatedFemales, total: updatedMales + updatedFemales };
+
+    //             // if()
+
+    //             return acc;
+    //         }, {})
+    //         console.log(most_populat)
+    //     })
 }
+FindHighestPopulationCounties()
